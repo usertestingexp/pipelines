a1eb42a303eca89dc6dca7cc7dd2606fb1105633
usertestingexp <bbox06187@gmail.com>
2024-12-27 16:21:14 -0500

    google_search.py

---
+++ examples/pipelines/providers/google_search.py
@@ -0,0 +1,42 @@
+"""
+title: Google GenAI Manifold Pipeline
+author: JC
+date: 2024-12-27
+version: 1.0
+license: MIT
+description: Search Tool Test
+requirements: google-generativeai
+"""
+
+from typing import List, Union, Iterator
+import os
+
+from pydantic import BaseModel, Field
+
+from google import genai
+from google.genai.types import Tool, GenerateContentConfig, GoogleSearch
+
+client = genai.Client()
+model_id = "gemini-2.0-flash-exp"
+
+google_search_tool = Tool(
+    google_search = GoogleSearch()
+)
+
+response = client.models.generate_content(
+    model=model_id,
+    contents="When is the next total solar eclipse in the United States?",
+    config=GenerateContentConfig(
+        tools=[google_search_tool],
+        response_modalities=["TEXT"],
+    )
+)
+
+for each in response.candidates[0].content.parts:
+    print(each.text)
+# Example response:
+# The next total solar eclipse visible in the contiguous United States will be on ...
+
+# To get grounding metadata as web content.
+print(response.candidates[0].grounding_metadata.search_entry_point.rendered_content)
+
